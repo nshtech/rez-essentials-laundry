82e3095fbd085117e3ee3be692b46593d1c56165
Patrice Power <patricepower2021@u.northwestern.edu>
2020-06-10 20:52:11 -0400

    added dashboard template files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfTUULx93uJ8x9gZN1gmTCYFT9zTDz_Xc",
+  authDomain: "rez-laundry-app.firebaseapp.com",
+  databaseURL: "https://rez-laundry-app.firebaseio.com",
+  projectId: "rez-laundry-app",
+  storageBucket: "rez-laundry-app.appspot.com",
+  messagingSenderId: "453879510299",
+  appId: "1:453879510299:web:edbf22170111e441500fbe",
+  measurementId: "G-VJM4T1CKYX"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
