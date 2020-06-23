4d4ecb3c9ec7bdd70222de5513d9a49304bfff8c
Caroline Lobel <57736173+carolinelobel@users.noreply.github.com>
2020-06-23 15:00:23 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,6 +11,9 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+
 import '../App.css';
 
 import firebase from 'firebase/app';
@@ -96,9 +99,17 @@ function Copyright() {
     rezbutton: {
       backgroundColor: '#6a09a4',
       color: 'white'
-    }
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '40ch',
+    },
 
   }));
+
+  
+
 
   export default function Profile() {
     const classes = useStyles();
@@ -111,6 +122,7 @@ function Copyright() {
     const handleDrawerClose = () => {
       setOpen(false);
     };
+    const [edit,setEdit] = React.useState(false);
 
     useEffect(() => {
       const db = firebase.database().ref().child('/customers/001');
@@ -125,10 +137,85 @@ function Copyright() {
       return () => { db.off('value', getCustomer); };
     }, []);
 
+    //HANDLE PHONE AND EMAIL UPDATE
+    function handleInputChange(event) {
+      const target=event.target;
+      const name = target.name;
+      const value = target.value;
+      if (name==="phone" && value.length===12) {
+        firebase.database().ref('/customers/'+customerinfo.id+'/phone').set(value);
+        customerinfo.phone = value;
+        console.log('database phone updated');
+      }
+      else if (name==="email" && value.includes("@") && value.includes('.')) {
+        firebase.database().ref('/customers/'+customerinfo.id+'/email').set(value);
+        customerinfo.email = value;
+        console.log('database email updated')
+      } 
+    }
 
-
-    return (
+    {/*EDIT MODE */}
+    if (edit) {
+      return (
+        <div className={classes.root}>
+  
+        <main className="classes.content">
+        <Container maxWidth="lg" className={classes.container}>
+            <Grid container spacing={1} classes={classes.grid} direction="row">
+              {/* Account Info */}
+              <Grid item xs={12} className={classes.subgrid}>
+                <Card>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2" className={classes.cardtitle}>
+                      <PersonIcon/>User Information
+                    </Typography>
+                    <Typography gutterBottom variant="body1" component="p">
+                        <b>Customer ID:</b> {customerinfo.id} <br/>
+                        <b>Residential Hall:</b> {customerinfo.reshall} <br/>
+                      <b>Current Plan:</b> School Year 2019-2020 <br/>
+                        <b>Weight Limit:</b> {customerinfo.maxweight} lb/week
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+              {/* Contact Information */}
+              <Grid item xs={12}>
+                  <Card className={classes.paper}>
+                    <Typography gutterBottom variant="h5" component="h2" className={classes.cardtitle}>
+                      <PermContactCalendarIcon/>Contact Information <Button size="small" variant="contained" className={classes.rezbutton} onClick={() => {setEdit(false)}}><b>SAVE</b></Button>
+                    </Typography>
+                    <Typography component="p" variant="body1">
+                      <b>Phone:</b> <Input name="phone" placeholder={customerinfo.phone} defaultValue={customerinfo.phone} className={classes.textField} onChange={handleInputChange}/> <br/>
+                      <b>Email:</b> <Input name="email" placeholder={customerinfo.email} defaultValue={customerinfo.email} className={classes.textField} onChange={handleInputChange}/>
+                    </Typography>
+                  </Card>
+                </Grid>
+              <Grid container item xs={12} className={classes.subgrid}>
+                {/* Laundry History */}
+                <Grid item xs={12}>
+                  <Card className={classes.paperbottom}>
+                    <Typography gutterBottom variant="h5" component="h2" className={classes.cardtitle}><LocalLaundryServiceIcon/>Account Trends</Typography>
+                    <Typography component="p" variant="body1" >
+                      My bag has been overweight <b>10/13</b> times this year.  I should increase my weekly weight limit!   <Button size="small" variant="contained" className={classes.rezbutton}><b>Upgrade Plan</b></Button>
+                    </Typography>
+                    
       
+                  </Card>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Box pt={4}>
+              <Copyright style={{ paddingTop: 3 }} />
+            </Box>
+          </Container>
+        </main>
+        </div>
+        
+      );
+    }
+   
+    else {
+    return (
       <div className={classes.root}>
 
       <main className="classes.content">
@@ -154,7 +241,7 @@ function Copyright() {
             <Grid item xs={12}>
                 <Card className={classes.paper}>
                   <Typography gutterBottom variant="h5" component="h2" className={classes.cardtitle}>
-                    <PermContactCalendarIcon/>Contact Information
+                    <PermContactCalendarIcon/>Contact Information <Button size="small" variant="contained" className={classes.rezbutton} onClick={() => {setEdit(true)}}><b>EDIT</b></Button>
                   </Typography>
                   <Typography component="p" variant="body1">
                     <b>Phone:</b> {customerinfo.phone} <br/>
@@ -163,19 +250,6 @@ function Copyright() {
                 </Card>
               </Grid>
             <Grid container item xs={12} className={classes.subgrid}>
-              
-              {/* Current Laundry Status 
-              <Grid item xs={12}>
-                <Card className={classes.paperbottom}>
-                  <Typography gutterBottom variant="h5" component="h2">Current Laundry Status</Typography>
-                  <Typography component="p" variant="body1" className="picked-up">
-                    picked up
-                  </Typography>
-                  <Typography  className="overweight" component="p" variant="body1">
-                    overweight
-                  </Typography>
-                </Card>
-              </Grid>*/}
               {/* Laundry History */}
               <Grid item xs={12}>
                 <Card className={classes.paperbottom}>
@@ -196,5 +270,5 @@ function Copyright() {
       </main>
       </div>
       
-    );
+    ); }
   }
